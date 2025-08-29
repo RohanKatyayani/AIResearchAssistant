7b5be0f2d1adcdeb7dcbf62317091d0666f0c7c4
Rohan Katyayani <60040641+RohanKatyayani@users.noreply.github.com>
2025-08-30 03:03:18 +0530

    Initial Commit

---
+++ AIResearchAssistant/AIResearchAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIResearchAssistantApp.swift
+//  AIResearchAssistant
+//
+//  Created by Rohan Katyayani on 30/08/25.
+//
+
+import SwiftUI
+
+@main
+struct AIResearchAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIResearchAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIResearchAssistant
+//
+//  Created by Rohan Katyayani on 30/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
